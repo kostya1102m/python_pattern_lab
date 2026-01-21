9873bf1359bce8040eae8e0a0ebf17a4cead0772
mercury <122353133+kostya1102m@users.noreply.github.com>
2026-01-21 21:14:25 +0900

    del

--- app/entities/order.py
+++
@@ -1,8 +0,0 @@
-from cart import Cart
-
-class Order:
-    def __init__(self, cart: Cart, discount: float, shipping_cost: float, final_amount: float):
-        self.cart = cart
-        self.discount = discount
-        self.shipping_cost = shipping_cost
-        self.final_amount = final_amount
